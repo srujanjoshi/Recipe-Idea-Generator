7585b944071b85d6759ed8e7a6f808c0a95ed0c8
Adeet Patel <adeet.patel@gmail.com>
2021-02-02 22:31:30 -0500

    Create Forgot Password page

---
+++ lib/pages/forgot_password.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:recipic/services/auth.dart';
+import 'package:recipic/models/constants.dart';
+
+class ForgotPassword extends StatefulWidget {
+
+  final Function toggleView;
+  ForgotPassword({this.toggleView});
+
+  @override
+  _ForgotPasswordState createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+
+  // text field state
+  String email = '';
+  String password = '';
+  String error = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return loading ? Loading() : Scaffold(
+      backgroundColor: Colors.grey[350],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[700],
+        elevation: 0.0,
+        title: Text('Forgot Password'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Sign In'),
+            onPressed: () {
+              widget.toggleView();
+            },
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                  onChanged: (val){
+                    setState(() => email = val);
+                  }
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                color: Colors.grey[800],
+                child: Text(
+                  'Send Password Reset Link',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+              SizedBox(height: 12),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
