68a6d48692b096a1ce88b1ec1cf35a924dcc170e
Adeet Patel <adeet.patel@gmail.com>
2021-02-03 22:22:28 -0500

    Convert ternary operator into conditional statement

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -23,93 +23,103 @@ class _SignInState extends State<SignIn> {
   String password = '';
   String error = '';
 
-  
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() : Scaffold(
-      backgroundColor: Colors.grey[350],
-      appBar: AppBar(
-        backgroundColor: Colors.grey[700],
-        elevation: 0.0,
-        title: Text('Sign In Page'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            onPressed: () {
-              widget.toggleView();
-            },
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20),
-              TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                  onChanged: (val){
-                    setState(() => email = val);
-                  }
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                  validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  }
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  RaisedButton(
-                    color: Colors.grey[800],
-                    child: Text(
-                      'Sign In',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()){
-                        setState(() => loading = true);
-                        dynamic result = await _auth.signInWithEmailAndPassword(email, password);
-                        if(result == null) {
-                          setState(() {
-                            error = 'could not sign in with those credentials';
-                            loading = false;
-                          });
-                        }
-                      }
-                    },
-                  ),
-                  SizedBox(width: 10),
-                  RaisedButton(
-                      child: Text(
-                          "Forgot Password",
-                          style: TextStyle(color: Colors.white)
-                      ),
+    if (loading) {
+      return Loading();
+    }
+    else {
+      return Scaffold(
+        backgroundColor: Colors.grey[350],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[700],
+          elevation: 0.0,
+          title: Text('Sign In Page'),
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('Register'),
+              onPressed: () {
+                widget.toggleView();
+              },
+            ),
+          ],
+        ),
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20),
+                TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                    validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    }
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                    decoration: textInputDecoration.copyWith(
+                        hintText: 'Password'),
+                    validator: (val) =>
+                    val.length < 6
+                        ? 'Enter a password 6+ chars long'
+                        : null,
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    }
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RaisedButton(
                       color: Colors.grey[800],
-                      onPressed: () {
-                        setState(() {});
-                      }
-                  )
-                ],
-              ),
-              SizedBox(height: 12),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14),
-              ),
-            ],
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() => loading = true);
+                          dynamic result = await _auth
+                              .signInWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error =
+                              'could not sign in with those credentials';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(width: 10),
+                    RaisedButton(
+                        child: Text(
+                            "Forgot Password",
+                            style: TextStyle(color: Colors.white)
+                        ),
+                        color: Colors.grey[800],
+                        onPressed: () {
+                          setState(() {});
+                        }
+                    )
+                  ],
+                ),
+                SizedBox(height: 12),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
